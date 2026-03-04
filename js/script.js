f878a23c5b9654ee8ada3268544c3b8ad98b2d8e
Dodzi Keglo <stephanekeglo@gmail.com>
2026-03-04 22:37:45 +0000

    Improve CSS selectors specificity to avoid css rules conflicts

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const header = document.querySelector('header')
-    const toggleMobileMenu = document.querySelector("#mobile-menu-toggle")
-    const toggleMobileMenuIcon = document.querySelector("#mobile-menu-toggle i")
-    const menuLinks = document.querySelector(".menu-links")
-    const menuLinksItem = document.querySelector(".menu-links ul")
+    const mainHeader = document.getElementById("main-header")
+    const mobileMenuToggle = document.getElementById("mobile-menu-toggle")
+    const mobileMenuToggleIcon = document.getElementById("mobile-menu-toggle-icon")
+    const mainMenu = document.getElementById("main-menu")
+    const mainMenuLinks = document.getElementById("main-menu-links")
 
-    menuLinksItem.addEventListener("click", function () {
-        menuLinks.classList.toggle("mobile-menu")
-        toggleMobileMenuIcon.classList.replace("fa-xmark", "fa-bars")
+    mainMenuLinks.addEventListener("click", function () {
+        mainMenu.classList.toggle("mobile-menu")
+        mobileMenuToggleIcon.classList.replace("fa-xmark", "fa-bars")
     })
     
-    toggleMobileMenu.addEventListener("click", function () {
-        menuLinks.classList.toggle("mobile-menu")
+    mobileMenuToggle.addEventListener("click", function () {
+        mainMenu.classList.toggle("mobile-menu")
         
-        if (toggleMobileMenuIcon.classList.contains("fa-bars")) {
-            toggleMobileMenuIcon.classList.replace("fa-bars", "fa-xmark")
+        if (mobileMenuToggleIcon.classList.contains("fa-bars")) {
+            mobileMenuToggleIcon.classList.replace("fa-bars", "fa-xmark")
         } else {
-            toggleMobileMenuIcon.classList.replace("fa-xmark", "fa-bars")
+            mobileMenuToggleIcon.classList.replace("fa-xmark", "fa-bars")
         }
     })
     
-    function highlightHeader () {
+    function highlightMainHeader () {
         if (window.scrollY > 0) {
-            header.classList.add("highlight")
+            mainHeader.classList.add("highlight")
         } else {
-            header.classList.remove("highlight")
+            mainHeader.classList.remove("highlight")
         }
     }
 
-    window.addEventListener("scroll", highlightHeader)
+    window.addEventListener("scroll", highlightMainHeader)
 })
